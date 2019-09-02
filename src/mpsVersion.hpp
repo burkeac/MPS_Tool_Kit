726f3b95ef310b89820bc3c0cefa62e4ab93484a
Adam Burke <adam.burke603@gmail.com>
2019-09-02 13:18:24 -0400

    Added version .hpp and function

---
+++ src/mpsVersion.hpp
@@ -0,0 +1,10 @@
+#ifndef mpsVersion_hpp
+#define mpsVersion_hpp
+
+#include <iostream>
+
+void printVersion(){
+    std::cout << "MPS Tool Kit Version: 1.0 Alpha Development" << std::endl;
+}
+
+#endif
