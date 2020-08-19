f59ae97c006346035ab367aa2e34e6c24248e1d6
Adam Burke <adam.burke603@gmail.com>
2020-08-18 23:49:23 -0400

    Added pybind11 to thirdparty lib directory

--- appSrc/testProgram.cpp
+++ appSrc/testProgram.cpp
@@ -4,9 +4,27 @@
 #include <iostream>
 #include "utilities.hpp"
 
-int main(){
+using namespace std;
 
-    MPS::CSVreader file("testFilePath");
-    std::cout << file.filePath << std::endl;
+int main(int argc, char* argv[]){
+
+    MPS::CSVreader data(argv[1]);
+
+    try{
+    data.read(false);
+    } catch (string err) {
+        cout << err << endl;
+        return EXIT_FAILURE;
+    }
+
+
+    auto myData = data.getRef();
+
+    myData[0][1] = "test";
+
+    std::cout << data.at(0,1) << std::endl;
+
+    std::cout << data[0][1] << std::endl;
+
     return EXIT_SUCCESS;
 }
