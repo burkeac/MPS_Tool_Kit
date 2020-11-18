88ea36fe70c1cc0159d163be40949b33fd87aafb
Adam Burke <adam.burke603@gmail.com>
2020-11-17 22:57:54 -0500

    inital commit of the LUT3D class and LUT files

---
+++ libSrc/Color/LUT.cpp
@@ -0,0 +1,443 @@
+// LUT.cpp
+// Adam C. Burke (adam.burke603@gmail.com)
+// October 13, 2020
+
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include "LUT.hpp"
+#include "utilities.hpp"
+ 
+enum{ red, green, blue };
+
+namespace MPS{
+
+LUT3D::LUT3D(const uint8_t& numNodes)
+    : numNodes(numNodes){
+    _Allocate3D_Data();
+}
+LUT3D::LUT3D(){};
+
+void LUT3D::_Allocate3D_Data(){
+    typedef std::vector<tripletF> row;
+    typedef std::vector<row> plane;
+    typedef std::vector<plane> cube;
+
+    // incilize the data structure
+    _data = LUT3D_Data(numNodes,plane(numNodes, row(numNodes)));
+
+    _NodeValueLUT.reserve(numNodes);
+    for(int i=0; i<numNodes; i++){
+        _NodeValueLUT.push_back((float )i / (numNodes-1));
+    }
+
+}
+
+void LUT3D::Generate_3DLUT_Nodes(){
+    float spacing = 1.0 / ((float)numNodes-1.0);
+
+    for(int R=0; R<numNodes; R++){
+        for(int G=0; G<numNodes; G++){
+            for(int B=0; B<numNodes; B++){
+                _data[R][G][B][red] = R * spacing;
+                _data[R][G][B][green] = G * spacing;
+                _data[R][G][B][blue] = B * spacing;
+            }
+        }
+    }
+}
+
+void LUT3D::Write2CSV(const std::string& filePath, 
+                      bool writeHeaders, 
+                      uint16_t scale_bitdepth){
+    
+    std::fstream fileOut(filePath, std::ios::out);
+    if(fileOut.good()){
+        if(writeHeaders)
+            fileOut << "R,G,B" << std::endl;
+
+        if(!scale_bitdepth){
+            for(int R=0; R<numNodes; R++){
+                for(int G=0; G<numNodes; G++){
+                    for(int B=0; B<numNodes; B++){
+                        fileOut << _data[R][G][B][red] << "," 
+                                << _data[R][G][B][green] << "," 
+                                << _data[R][G][B][blue] << "\n";
+                    }
+                }
+            }
+        }else{
+            int NumCVs = pow(2, scale_bitdepth);
+            for(int R=0; R<numNodes; R++){
+                for(int G=0; G<numNodes; G++){
+                    for(int B=0; B<numNodes; B++){
+                        fileOut << ((_data[R][G][B][red]   * NumCVs)   < (NumCVs-1) 
+                            ? (_data[R][G][B][red]   * NumCVs) : (NumCVs-1)) 
+                        << ","  << ((_data[R][G][B][green] * NumCVs)   < (NumCVs-1) 
+                            ? (_data[R][G][B][green] * NumCVs) : (NumCVs-1)) 
+                        << ","  << ((_data[R][G][B][blue]  * NumCVs)   < (NumCVs-1) 
+                            ? (_data[R][G][B][blue]  * NumCVs) : (NumCVs-1)) 
+                        << "\n";
+                    }
+                }
+            }
+        }
+
+        fileOut.flush();
+        fileOut.close();
+    }
+}
+
+void LUT3D::Write2CUBE_Adobe(const std::string& filePath, 
+                             const CUBE_Params* Params){
+
+    LUT3D::CUBE_Params l_Params;
+
+    // write any and all keywords to file
+    std::fstream fileOut(filePath, std::ios::out);
+    if(fileOut.good()){
+        if(Params){ // optional keywords
+            if(Params->TITLE != "")
+                fileOut << "TITLE " << Params->TITLE << "\n";
+
+            tripletF DefaultMin = {0,0,0};
+            if(Params->DOMAIN_MIN != DefaultMin)
+                fileOut << "DOMAIN_MIN " 
+                        << Params->DOMAIN_MIN[0] << " " 
+                        << Params->DOMAIN_MIN[1] << " "
+                        << Params->DOMAIN_MIN[2] << "\n";
+
+            tripletF DefaultMax = {1,1,1};
+            if(Params->DOMAIN_MAX != DefaultMax)
+                fileOut << "DOMAIN_MAX " 
+                        << Params->DOMAIN_MAX[0] << " " 
+                        << Params->DOMAIN_MAX[1] << " "
+                        << Params->DOMAIN_MAX[2] << "\n";
+
+            l_Params = *Params;
+        }
+
+        // required keywords
+        fileOut << "LUT_3D_SIZE " << (int)numNodes << "\n";
+
+        // write the code values out...
+        for(int B=0; B<numNodes; B++){
+            for(int G=0; G<numNodes; G++){
+                for(int R=0; R<numNodes; R++){
+                    fileOut << _data[R][G][B][red]   * (l_Params.DOMAIN_MAX[0] - l_Params.DOMAIN_MIN[0]) 
+                                + l_Params.DOMAIN_MIN[0] << " " 
+                            << _data[R][G][B][green] * (l_Params.DOMAIN_MAX[1] - l_Params.DOMAIN_MIN[1]) 
+                                + l_Params.DOMAIN_MIN[1] << " " 
+                            << _data[R][G][B][blue]  * (l_Params.DOMAIN_MAX[2] - l_Params.DOMAIN_MIN[2]) 
+                                + l_Params.DOMAIN_MIN[2] << "\n"; 
+                }
+            }
+        }
+
+        //closet the file
+        fileOut.flush();
+        fileOut.close();
+    }
+}
+
+void LUT3D::Write2CUBE_BM(const std::string& filePath, 
+                          const CUBE_Params* Params){
+
+    LUT3D::CUBE_Params l_Params;
+
+    // write any and all keywords to file
+    std::fstream fileOut(filePath, std::ios::out);
+    if(fileOut.good()){
+        if(Params){ // optional keywords
+            if(Params->TITLE != "")
+                fileOut << "TITLE " << Params->TITLE << "\n";
+
+            if( (Params->DOMAIN_MIN[0] != 0) || (Params->DOMAIN_MAX[0] != 1) )
+                fileOut << "LUT_3D_INPUT_RANGE " 
+                        << Params->DOMAIN_MIN[0] << " "
+                        << Params->DOMAIN_MAX[0] << "\n";
+
+            l_Params = *Params;
+        }
+
+        // required keywords
+        fileOut << "LUT_3D_SIZE " << (int)numNodes << "\n";
+
+        // write the code values out...
+        for(int B=0; B<numNodes; B++){
+            for(int G=0; G<numNodes; G++){
+                for(int R=0; R<numNodes; R++){
+                    fileOut << _data[R][G][B][red]   * (l_Params.DOMAIN_MAX[0] - l_Params.DOMAIN_MIN[0]) 
+                                + l_Params.DOMAIN_MIN[0] << " " 
+                            << _data[R][G][B][green] * (l_Params.DOMAIN_MAX[1] - l_Params.DOMAIN_MIN[1]) 
+                                + l_Params.DOMAIN_MIN[1] << " " 
+                            << _data[R][G][B][blue]  * (l_Params.DOMAIN_MAX[2] - l_Params.DOMAIN_MIN[2]) 
+                                + l_Params.DOMAIN_MIN[2] << "\n"; 
+                }
+            }
+        }
+
+        //closet the file
+        fileOut.flush();
+        fileOut.close();
+    }
+}
+
+void LUT3D::ReadFromCSV(const std::string& filePath, 
+                        uint8_t scale_bitdepth){
+
+    if(numNodes == 0){
+        throw(std::invalid_argument("Number of nodes must be defined"));
+        return;
+    }
+    CSVreader CSVFile(filePath, true);
+    auto csvdata = CSVFile.getRef();
+    bool hasHeaders = false;
+    int line = 0;
+    for(int i=0; i<3; i++){
+        for(auto c : csvdata[0][0]){
+            if( !isdigit(c) ){
+                line = 1;
+                hasHeaders = true;
+                break;
+            }
+        }
+    }
+
+    if(scale_bitdepth == 0){
+        for(int R=0; R<numNodes; R++){
+            for(int G=0; G<numNodes; G++){
+                for(int B=0; B<numNodes; B++){
+                    try{
+                        _data[R][G][B][0] = std::stof(csvdata[line][0]);
+                        _data[R][G][B][1] = std::stof(csvdata[line][1]);
+                        _data[R][G][B][2] = std::stof(csvdata[line][2]);
+                        line++;
+                    }catch(...){
+                        throw(std::invalid_argument("Cannot convert value on line: " + 
+                            std::to_string(line) + " to numeric value. Aborting"));
+                    }
+                }
+            }
+        }
+    }else{
+    // if scaling is needed
+        float scaleFactor = pow(2, scale_bitdepth) - 1;
+
+        for(int R=0; R<numNodes; R++){
+            for(int G=0; G<numNodes; G++){
+                for(int B=0; B<numNodes; B++){
+                    try{
+                        _data[R][G][B][0] = std::stof(csvdata[line][0]) / scaleFactor;
+                        _data[R][G][B][1] = std::stof(csvdata[line][1]) / scaleFactor;
+                        _data[R][G][B][2] = std::stof(csvdata[line][2]) / scaleFactor;
+                        line++;
+                        
+                    }catch(...){
+                        throw(std::invalid_argument("Cannot convert value on line: " + 
+                            std::to_string(line) + " to numeric value. Aborting"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+// read from cube file
+void LUT3D::ReadFromCubeFile(const std::string& filePath,
+                             CUBE_Params* Params){
+    std::fstream fileIn(filePath, std::ios::in);
+    if(!fileIn.is_open()){
+        throw(std::invalid_argument("Can't open file specified."));
+        return;
+    }
+
+    tripletF DOMAIN_MIN = {0,0,0};
+    tripletF DOMAIN_MAX = {1,1,1};
+    bool NormalizationNeeded = false;
+    
+
+    int R, G, B = 0;
+
+    std::string line;
+    std::string value;
+
+    while(getline(fileIn, line)){
+        std::stringstream lineS(line);
+
+        lineS >> value;
+        
+        // if comment line: skip
+        if(value[0] == '#') continue; 
+
+        if(value == "LUT_3D_SIZE"){
+            lineS >> value;
+            numNodes = stof(value);
+            _Allocate3D_Data();
+        }
+        
+        else if(value == "TITLE"){
+            std::string title;
+            getline(lineS, value, '\n');
+            if(Params)
+                Params->TITLE = MPS::strip(value);
+        }
+
+        else if(value == "DOMAIN_MIN"){
+            lineS >> value;
+            DOMAIN_MIN[red] = stof(value);
+            if(Params) Params->DOMAIN_MIN[red] = stof(value);
+
+            lineS >> value;
+            DOMAIN_MIN[green] = stof(value);
+            if(Params) Params->DOMAIN_MIN[green] = stof(value);
+
+            lineS >> value;
+            DOMAIN_MIN[blue] = stof(value);
+            if(Params) Params->DOMAIN_MIN[blue] = stof(value);
+        }
+
+        else if(value == "DOMAIN_MAX"){
+            lineS >> value;
+            DOMAIN_MAX[red] = stof(value);
+            if(Params) Params->DOMAIN_MAX[red] = stof(value);
+
+            lineS >> value;
+            DOMAIN_MAX[green] = stof(value);
+            if(Params) Params->DOMAIN_MAX[green] = stof(value);
+
+            lineS >> value;
+            DOMAIN_MAX[blue] = stof(value);
+            if(Params) Params->DOMAIN_MAX[blue] = stof(value);
+        }
+
+        else if (value == "LUT_3D_INPUT_RANGE"){
+            lineS >> value;
+            DOMAIN_MIN[red] = stof(value);
+            DOMAIN_MIN[green] = stof(value);
+            DOMAIN_MIN[blue] = stof(value);
+            if(Params) Params->DOMAIN_MIN[red] = stof(value);
+            if(Params) Params->DOMAIN_MIN[green] = stof(value);
+            if(Params) Params->DOMAIN_MIN[blue] = stof(value);
+
+            lineS >> value;
+            DOMAIN_MAX[red] = stof(value);
+            DOMAIN_MAX[green] = stof(value);
+            DOMAIN_MAX[blue] = stof(value);
+            if(Params) Params->DOMAIN_MAX[red] = stof(value);
+            if(Params) Params->DOMAIN_MAX[green] = stof(value);
+            if(Params) Params->DOMAIN_MAX[blue] = stof(value);
+        }
+
+        // parse values
+        else {
+            _data[R][G][B][red] =   (stof(value) - DOMAIN_MIN[0]) / (DOMAIN_MAX[0] - DOMAIN_MIN[0]);
+            lineS >> value;
+            _data[R][G][B][green] = (stof(value) - DOMAIN_MIN[1]) / (DOMAIN_MAX[1] - DOMAIN_MIN[1]);
+            lineS >> value;
+            _data[R][G][B][blue] =  (stof(value) - DOMAIN_MIN[2]) / (DOMAIN_MAX[2] - DOMAIN_MIN[2]);
+        
+        
+            // increment channel indexes appropriatly
+            R++;
+            if(R >= numNodes){ G++; R = 0; }
+            if(G >= numNodes){ B++; G = 0; }
+            if(B >= numNodes){ break; }
+        }
+
+    }
+
+    fileIn.close();
+}
+
+inline float LUT3D::_getC(const tripletB node, const tripletF CV, const int chan){
+    float multFactor = numNodes -1;
+    
+    int a = node[0] ? std::ceil(CV[0] * multFactor) : std::floor(CV[0] * multFactor);
+    int b = node[1] ? std::ceil(CV[1] * multFactor) : std::floor(CV[1] * multFactor);
+    int c = node[2] ? std::ceil(CV[2] * multFactor) : std::floor(CV[2] * multFactor);
+    return _data[a][b][c][chan];
+}
+
+inline float LUT3D::_getScaleFactor(const float cord){
+    float multFactor = numNodes - 1; 
+
+    float din = _NodeValueLUT[std::ceil(cord * multFactor)] -
+                 _NodeValueLUT[std::floor(cord * multFactor)];
+    if(din)
+        return ( (cord - _NodeValueLUT[std::floor(cord * multFactor)]) / din );
+    else
+        return 0;
+}
+
+tripletF LUT3D::Interpolate_Trilin(const float R, const float G, const float B){
+    // interp red channel
+    float rC1 = _getC({false,false,false}, {R,G,B}, 0);
+    float rC2 = _getC({true,false,false},  {R,G,B}, red);
+    float rC3 = _getC({false,true,false},  {R,G,B}, red);
+    float rC4 = _getC({true,true,false},   {R,G,B}, red);
+    float rC5 = _getC({false,false,true},  {R,G,B}, red);
+    float rC6 = _getC({true, false, true}, {R,G,B}, red);
+    float rC7 = _getC({false,true,true},   {R,G,B}, red);
+    float rC8 = _getC({true,true,true},    {R,G,B}, red);
+
+    
+    float rC12 = _getScaleFactor(R) * (rC2-rC1) + rC1;
+    float rC34 = _getScaleFactor(R) * (rC4-rC3) + rC3;
+    float rC56 = _getScaleFactor(R) * (rC6-rC5) + rC5;
+    float rC78 = _getScaleFactor(R) * (rC8-rC7) + rC7;
+
+    float rC1234 = _getScaleFactor(G) * (rC34 - rC12) + rC12;
+    float rC5678 = _getScaleFactor(G) * (rC78 - rC56) + rC56;
+
+    float rC = _getScaleFactor(B) * (rC5678 - rC1234) + rC1234;
+    
+    // interp green channel
+    float gC1 = _getC({false,false,false}, {R,G,B}, green);
+    float gC2 = _getC({true,false,false},  {R,G,B}, green);
+    float gC3 = _getC({false,true,false},  {R,G,B}, green);
+    float gC4 = _getC({true,true,false},   {R,G,B}, green);
+    float gC5 = _getC({false,false,true},  {R,G,B}, green);
+    float gC6 = _getC({true, false, true}, {R,G,B}, green);
+    float gC7 = _getC({false,true,true},   {R,G,B}, green);
+    float gC8 = _getC({true,true,true},    {R,G,B}, green);
+
+    float gC12 = _getScaleFactor(R) * (gC2-gC1) + gC1;
+    float gC34 = _getScaleFactor(R) * (gC4-gC3) + gC3;
+    float gC56 = _getScaleFactor(R) * (gC6-gC5) + gC5;
+    float gC78 = _getScaleFactor(R) * (gC8-gC7) + gC7;
+
+    float gC1234 = _getScaleFactor(G) * (gC34 - gC12) + gC12;
+    float gC5678 = _getScaleFactor(G) * (gC78 - gC56) + gC56;
+
+    float gC = _getScaleFactor(B) * (gC5678 - gC1234) + gC1234;
+
+    // interp blue channel
+    float bC1 = _getC({false,false,false}, {R,G,B}, blue);
+    float bC2 = _getC({true,false,false},  {R,G,B}, blue);
+    float bC3 = _getC({false,true,false},  {R,G,B}, blue);
+    float bC4 = _getC({true,true,false},   {R,G,B}, blue);
+    float bC5 = _getC({false,false,true},  {R,G,B}, blue);
+    float bC6 = _getC({true, false, true}, {R,G,B}, blue);
+    float bC7 = _getC({false,true,true},   {R,G,B}, blue);
+    float bC8 = _getC({true,true,true},    {R,G,B}, blue);
+
+    float bC12 = _getScaleFactor(R) * (bC2-bC1) + bC1;
+    float bC34 = _getScaleFactor(R) * (bC4-bC3) + bC3;
+    float bC56 = _getScaleFactor(R) * (bC6-bC5) + bC5;
+    float bC78 = _getScaleFactor(R) * (bC8-bC7) + bC7;
+
+    float bC1234 = _getScaleFactor(G) * (bC34 - bC12) + bC12;
+    float bC5678 = _getScaleFactor(G) * (bC78 - bC56) + bC56;
+
+    float bC = _getScaleFactor(B) * (bC5678 - bC1234) + bC1234;
+
+
+    return {rC,gC,bC};
+}
+
+} // end namespace
---
+++ libSrc/Color/LUT.hpp
@@ -0,0 +1,133 @@
+// LUT.hpp
+// Adam C. Burke (adam.burke603@gmail.com)
+// October 13, 2020
+
+#pragma once
+#include <vector>
+#include "macro_typedef.hpp"
+
+namespace MPS {
+
+class LUT3D {
+    private:
+    /// Internal Data structure for the LUT3D class.
+    /*  
+    Data should be stored in RGB index order. Access to the "output" code values
+    should be accessed as such: LUT3D_Data[in_R][in_G][in_B]. This will return a 
+    3 element array in the out_R out_G out_B index order.
+    */
+    typedef std::vector<std::vector<std::vector<tripletF>>> LUT3D_Data;
+
+    // data is stored in a 3D array. data[R][G][B] returns the new codevalues of RGB 
+    LUT3D_Data _data;
+
+    // Used for interpolation normalization
+    std::vector<float> _NodeValueLUT;
+
+    /// Allocates the interal data structure
+    void _Allocate3D_Data();
+    inline float _getC(const tripletB node, const tripletF CV, const int chan);
+    inline float _getScaleFactor(const float cord);
+
+    public:
+    uint8_t numNodes;
+
+    struct CUBE_Params{
+        std::string TITLE = "";
+        tripletF DOMAIN_MIN = {0.0f,0.0f,0.0f};
+        tripletF DOMAIN_MAX = {1.0f,1.0f,1.0f};
+    };
+
+    LUT3D();
+
+    /**
+     * \note It is recomended that the number of nodes follows the sequence: 2^x+1. 
+     * Example: nodes = {9, 17, 33, 65}. This is to ensure uniform spacing between code values.
+     * */
+    LUT3D(const uint8_t& numNodes);
+
+    /// Generates the inital code values
+    /** 
+     * Without manipulation, this would generate a "null" or "identity" LUT 
+     * where input = output 
+     */
+    void Generate_3DLUT_Nodes();
+
+    /// Writes the LUT to a CSV file.
+    /**
+     * Writes the data to a CSV file in the RGB format, where the blue channel
+     * changes most frequently. 
+     * @param [in] filePath         The path of the file to be written.
+     * @param [in] writeHeaders     Writes "R,G,B" headers as the first row. Default = true, 
+     * @param [in] scale_bitdepth   Scales the code values to the specified integer bitdepth.
+     *                              Scaling is off when scale_bitdepth is 0
+     * */
+    void Write2CSV(const std::string& filePath, 
+                   bool writeHeaders = true, 
+                    uint16_t scale_bitdepth = 0);
+
+    /// Writes to an Adobe(C)/IRIDAS(C) .cube file
+    /**
+     * Writes the data to an Adobe/IRIDAS cube file. 
+     * \param [in] filePath  The path of the file to be written.
+     * \param [in] Params    Pointer to an object containing the optional paramaters.
+     *                       Default value = NULL.
+     * 
+     * Reference: \n
+     * https://wwwimages2.adobe.com/content/dam/acom/en/products/speedgrade/cc/pdfs/cube-lut-specification-1.0.pdf
+     * */
+    void Write2CUBE_Adobe(const std::string& filePath, 
+                          const CUBE_Params* Params = NULL);
+/// Writes to an Adobe(C)/IRIDAS(C) .cube file
+    /**
+     * Writes the data to a Black Magic cube file. 
+     * \param [in] filePath  The path of the file to be written.
+     * \param [in] Params    Pointer to an object containing the optional paramaters.
+     *                       Default value = NULL.
+     * */
+    void Write2CUBE_BM(const std::string& filePath, 
+                       const CUBE_Params* Params = NULL);
+
+    /// Reads from a specified CSV file containing 3D LUT data.
+    /** 
+     * The data within the CSV file should be in RGB format, where the blue channel
+     * changes most frequently. 
+     * 
+     * \param [in] FilePath The path of the file to be read.
+     * \param [in] scale_bitdepth The bitdepth the code values must be scaled down from.
+     *             Scaling is ignored when scale_bitdepth is 0. In this case, values should 
+     *             range between 0 and 1 (float).
+     * */
+    void ReadFromCSV(const std::string& filePath,
+                     uint8_t scale_bitdepth = 0);
+
+    /// Reads from a specified cube file (Adobe or BM) containting the 3D LUT data.
+    /** 
+     * Reads data from a 3D cube file. The LUT size is determined based on the keywords within
+     * the file. This method will work for both Adobe cube files and Black Magic Cube Files.
+     * @param [in] filePath The file that should be read.
+     * @param [in] Params If provided, the optional keyword paramaters will be filled for later usage.
+     * */
+    void ReadFromCubeFile(const std::string& filePath,
+                          CUBE_Params* Params = NULL);
+
+
+
+    // wite to image
+
+    // read from image
+
+    /// Returns the interpolated values of the supplied 3D LUT using the trilinear interpolation method.
+    /**
+     * Uses the trilinear interpolation method to generate output cove values. All inputs should be normalized 
+     * between 0-1. 
+     * @param [in] R the input code value for the red channel.
+     * @param [in] G the input code value for the green channel.
+     * @param [in] B the input code value for the blue channel.
+     * @return An array of floating point numbers in the order of [RGB]
+     * 
+     * */
+    tripletF Interpolate_Trilin(const float R, const float G, const float B);
+
+};
+}
